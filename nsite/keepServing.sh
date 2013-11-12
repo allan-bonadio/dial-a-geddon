2c397ad9dcf47cb151dd74500b76f94d543a61cb
allan-bonadio <orgmgr@tactileint.org>
2013-11-12 21:20:07 +0000

    comment

--- nsite/keepServing.sh
+++ nsite/keepServing.sh
@@ -10,6 +10,7 @@ runOnce ( )
 	
 	# --dev sets app to include js old fashioned way, instead of inline
 	# --debug works with node-inspector --debug-brk causes stop at first stmt
+	# just do a node-inspector & somewhere.
 	#node --debug app  --dev
 	#node --debug-brk app --dev
 	node --debug-brk app 
